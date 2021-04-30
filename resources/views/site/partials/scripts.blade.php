d815f9a5c429d89e2c32b095aab45ea7a11998ab
Brian Mweu <33147760+BrianMwengi@users.noreply.github.com>
2021-04-30 11:05:43 +0300

    added secure_asset

--- resources/views/site/partials/scripts.blade.php
+++ resources/views/site/partials/scripts.blade.php
@@ -1,14 +1,14 @@
 <!-- ##### All Javascript Script ##### -->
 <!-- jQuery-2.2.4 js -->
-<script src="{{ asset('js/jquery/jquery-2.2.4.min.js') }}"></script>
+<script src="{{ secure_asset('js/jquery/jquery-2.2.4.min.js') }}"></script>
 <!-- Popper js -->
-<script src="{{ asset('js/bootstrap/popper.min.js') }}"></script>
+<script src="{{ secure_asset('js/bootstrap/popper.min.js') }}"></script>
 <!-- Bootstrap js -->
-<script src="{{ asset('js/bootstrap/bootstrap.min.js') }}"></script>
+<script src="{{ secure_asset('js/bootstrap/bootstrap.min.js') }}"></script>
 <!-- All Plugins js -->
-<script src="{{ asset('js/plugins/plugins.js') }}"></script>
+<script src="{{ secure_asset('js/plugins/plugins.js') }}"></script>
 <!-- Active js -->
-<script src="{{ asset('js/active.js') }}"></script>
+<script src="{{ secure_asset('js/active.js') }}"></script>
 
 <!--Html5 gallery js-->
-<script type="text/javascript" src="{{ asset('js/html5gallery.js')}}"></script>
+<script type="text/javascript" src="{{ secure_asset('js/html5gallery.js')}}"></script>
