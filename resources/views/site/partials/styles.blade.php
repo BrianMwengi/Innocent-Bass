2ccc49b605a2ed442320760744b270918859d94a
Brian Mweu <33147760+BrianMwengi@users.noreply.github.com>
2021-04-30 11:04:15 +0300

    added secure_asset

--- resources/views/site/partials/styles.blade.php
+++ resources/views/site/partials/styles.blade.php
@@ -1,11 +1,11 @@
 <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/core-img/favicon.ico') }}">
-<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('css/classy-nav.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('css/owl.carousel.min.css') }}" rel="stylesheet" type="text/css"/>
-<link href="{{ asset('css/animate.css') }}" rel="stylesheet">
-<link href="{{ asset('css/magnific-popup.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('css/audioplayer.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('css/one-music-icon.css') }}" rel="stylesheet" type="text/css">
-<link rel="stylesheet"  type="text/css" href="{{ asset('css/style.css') }}" >
+<link href="{{ secure_asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ secure_asset('css/classy-nav.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ secure_asset('css/owl.carousel.min.css') }}" rel="stylesheet" type="text/css"/>
+<link href="{{ secure_asset('css/animate.css') }}" rel="stylesheet">
+<link href="{{ secure_asset('css/magnific-popup.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ secure_asset('css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ secure_asset('css/audioplayer.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ secure_asset('css/one-music-icon.css') }}" rel="stylesheet" type="text/css">
+<link rel="stylesheet"  type="text/css" href="{{ secure_asset('css/style.css') }}" >
  
